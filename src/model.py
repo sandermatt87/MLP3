5f5d6f04369f23260d13c69ce5ae2d76cf78be49
sandermatt87 <sandermatt@gmx.ch>
2016-12-02 17:55:16 +0100

    individual classification

--- src/model.py
+++ src/model.py
@@ -16,7 +16,7 @@ class model:
 	ntest=0
 	train_features=None
 	test_features=None
-	predictor=None
+	predictor=[]
 	predictions=None
 	cv_predictions=None
 	fname_cache_train=None
@@ -39,7 +39,7 @@ class model:
 		self.check_prediction_cache()
 	
 	def train(self,targets,nsplits,nclasses):
-		new_targets=utils.to_single_class(targets)
+		#new_targets=utils.to_single_class(targets)
 		
 		print "training model with: ",self.fname_cache_train
 		if(self.prediction_cache_exists):
@@ -48,23 +48,23 @@ class model:
 			self.cv_score+=hamming_loss(targets,self.cv_predictions)
 			print "cv_hamming_loss: "+str(self.cv_score)
 		else:
-			self.predictor = OneVsRestClassifier(SVC(gamma=0.001/self.train_features.shape[1]))
+			for i in range(0,nclasses):
+				self.predictor.append(SVC(gamma=0.001/self.train_features.shape[1]))
 			np.random.seed(1231)
 			self.cv_predictions=np.copy(targets)*0
 			self.cv_score=0
 			kf = KFold(n_splits=nsplits)
 			for train, test in kf.split(self.train_features):
-				self.predictor.fit(self.train_features[train],new_targets[train])
-				print self.predictor.classes_
-				tmp=self.predictor.predict(self.train_features[test])
-				self.cv_predictions[test]=utils.to_multiple_classes(tmp,nclasses)
-				self.cv_score+=hamming_loss(targets[test],self.cv_predictions[test],labels=(0,1))
+				for i in range(0,nclasses):
+					self.predictor[i].fit(self.train_features[train],targets[train,i])
+					self.cv_predictions[test,i]=self.predictor[i].predict(self.train_features[test])
+				self.cv_score+=hamming_loss(targets[test],self.cv_predictions[test])
 			self.cv_score/=nsplits
 			print "cv_hamming_loss: "+str(self.cv_score)
-		
-			self.predictor.fit(self.train_features, new_targets)
+			for i in range(0,nclasses):
+				self.predictor[i].fit(self.train_features, targets[:,i])
 				
-		self.predict()
+		self.predict(nclasses)
 		
 	def get_features(self,path):
 		if(self.feature_cache_exists):
@@ -78,17 +78,14 @@ class model:
 			self.test_features=preprocess.remove_zero_variance(self.test_features,nonzeros)
 			self.cache_features()
 		
-	def predict(self):
+	def predict(self,nclasses):
 		self.check_prediction_cache()
 		if(self.prediction_cache_exists):
 			self.read_cache_predictions()
 		else:
-			if(self.train_features is None or self.train_features.shape[1]<2):
-				self.cv_predictions=np.zeros(self.ntrain)
-				self.predictions=np.zeros(self.ntest)
-				self.cache_predictions()
-			else:
-				self.predictions=self.predictor.predict(self.test_features)
+			self.predictions=np.zeros((self.ntest,nclasses))
+			for i in range(0,nclasses):
+				self.predictions[:,i]=self.predictor[i].predict(self.test_features)
 				self.cache_predictions()
 			
 	def cache_features(self):
