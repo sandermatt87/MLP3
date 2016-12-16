7d55f6a18c1d01569a1e9985cd9366ea67d3b9d5
sandermatt87 <sandermatt@gmx.ch>
2016-12-16 09:49:56 +0100

    bugfix cube number

--- src/init_models.py
+++ src/init_models.py
@@ -6,9 +6,9 @@ def init(ntrain,ntest,max_cubes,nseg, slack_mod1, slack_mod2, smoothening, nclas
     models=[]
 
     for seg in range(1,2):
-         for ncubes in range(0,1):
+         for ncubes in range(1,max_cubes+1):
              #                             (self,  ntrain,  ntest,  seg,  gamma_scale,   slack,       ncubes,   smoothening,  cname,                                    nclasses)
-             models.append(voxel_model.voxel_model(ntrain,  ntest,  seg,  0,             slack_mod1,  max_cubes,   smoothening,  "seg"+str(nseg)+"c"+str(max_cubes)+"voxels", nclasses))
-             models.append(distance_segmentation.distance_segmentation(ntrain,ntest,nseg,0,slack_mod2,max_cubes,"seg"+str(nseg)+"c"+str(max_cubes)+"distance", nclasses))
+             models.append(voxel_model.voxel_model(ntrain,  ntest,  seg,  0,             slack_mod1,  ncubes,   smoothening,  "seg"+str(nseg)+"c"+str(ncubes)+"voxels", nclasses))
+             models.append(distance_segmentation.distance_segmentation(ntrain,ntest,nseg,0,slack_mod2,ncubes,"seg"+str(nseg)+"c"+str(ncubes)+"distance", nclasses))
 
     return models
