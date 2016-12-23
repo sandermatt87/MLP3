bf6e66db53f53cdedb3b6acd006aef36dba287d9
sandermatt87 <sandermatt@gmx.ch>
2016-12-23 09:25:54 +0100

    updating the distance segmentation model

--- src/distance_segmentation.py
+++ src/distance_segmentation.py
@@ -1,11 +1,11 @@
-import model
-import parse
-import preprocess
 import nibabel as nib
 import numpy as np
 from sklearn.model_selection import KFold
 
 import preprocess
+import cube_predictor
+import model
+import parse
 
 #this model calculates the closest distance of each voxel to a voxel that is not part of the segment
 class distance_segmentation(model.model):
@@ -16,16 +16,18 @@ class distance_segmentation(model.model):
 	
 	def __init__(self,ntrain,ntest,seg,gamma_scale,slack,ncubes,cname,nclasses,invert=False):
 		model.model.__init__(self,ntrain,ntest,seg,gamma_scale,slack,cname,nclasses)
-		self.invert=invert
 		self.ncubes=ncubes
+		if(ncubes>1):
+			for i in range(0,nclasses):
+				self.predictor[i]=cube_predictor.cube_predictor(ncubes,gamma_scale[i],slack[i],cv_opt=True)
 		
 	def read_features(self,path):
 		tmp=[]
 		train_path=path+"set_train/c"+str(self.seg)+"train_"
+		print "reading train images"
 		for i in range(0,self.ntrain):
-			print "reading train image "+str(i)
 			filename=train_path+str(i+1)+".nii"
-			array=parse.voxels_from_image(filename,self.ncubes,smoothening=False)
+			array=parse.voxels_from_image(filename,smoothening=False)
 			if(self.invert):
 				array[array==0]=-1
 				array[array>0]=0
@@ -33,16 +35,16 @@ class distance_segmentation(model.model):
 			else:
 				array[array>0]=10000
 			array=closest_distance(array)
-			tmp.append(np.reshape(array,array.size))
+			tmp.append(preprocess.features1D(array,self.ncubes))
 		self.train_features=np.zeros((self.ntrain,tmp[0].size))
 		for i in range (0,self.ntrain):
 			self.train_features[i,:]=tmp[i]
 		tmp=[]
 		test_path=path+"set_test/c"+str(self.seg)+"test_"
+		print "reading test images"
 		for i in range(0,self.ntest):
-			print "reading test image "+str(i)
 			filename=test_path+str(i+1)+".nii"
-			array=parse.voxels_from_image(filename,self.ncubes,smoothening=False)
+			array=parse.voxels_from_image(filename,smoothening=False)
 			if(self.invert):
 				array[array==0]=-1
 				array[array>0]=0
@@ -50,10 +52,14 @@ class distance_segmentation(model.model):
 			else:
 				array[array>0]=10000
 			array=closest_distance(array)
-			tmp.append(np.reshape(array,array.size))
+			tmp.append(preprocess.features1D(array,self.ncubes))
 		self.test_features=np.zeros((self.ntest,tmp[0].size))
-		for i in range (0,self.ntest):
-			self.test_features[i,:]=tmp[i]
+		if(self.ncubes==1):
+			#Remove zero vaiance elements, to make the pca cheaper. This can only be done without cubes, since otherwise the format gets messed up
+			nonzeros=preprocess.get_nonzero_variance(self.train_features)
+			self.train_features=preprocess.remove_zero_variance(self.train_features,nonzeros)
+			self.test_features=preprocess.remove_zero_variance(self.test_features,nonzeros)
+		self.train_features,self.test_features=preprocess.segwise_pca(self.train_features,self.test_features,self.ncubes,self.ntrain,self.ntest)
 			
 def closest_distance(array):
 	xdim=array.shape[0]+2
