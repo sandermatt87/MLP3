d53821133c774ae60d2cd8fda70a80aec139ffdf
sandermatt87 <sandermatt@gmx.ch>
2016-12-30 01:17:23 +0100

    add fft model

---
+++ src/fft_model.py
@@ -0,0 +1,52 @@
+import nibabel as nib
+import numpy as np
+from sklearn.model_selection import KFold
+
+import model
+import parse
+import preprocess
+import cube_predictor
+
+#this model uses the voxels as input
+class fft_model(model.model):
+
+	ncubes=1
+	smoothening=-1
+
+	def __init__(self,ntrain,ntest,seg,gamma,slack,ncubes,smoothening,cname,nclasses):
+		model.model.__init__(self,ntrain,ntest,seg,gamma,slack,cname,nclasses)
+		self.ncubes=ncubes
+		self.smoothening=smoothening
+		if(ncubes>1):
+			for i in range(0,nclasses):
+				self.predictor[i]=cube_predictor.cube_predictor(ncubes,gamma[i],slack[i],cv_opt=False)
+		
+	def read_features(self,path):
+		tmp=[]
+		train_path=path+"set_train/c"+str(self.seg)+"train_"
+		print "reading train images"
+		for i in range(0,self.ntrain):
+			filename=train_path+str(i+1)+".nii"
+			array=parse.voxels_from_image(filename,smoothening_width=self.smoothening,stride=[3,3,3])
+			tmp.append(preprocess.fft_features1D(array,self.ncubes))
+		self.train_features=np.zeros((self.ntrain,tmp[0].size))
+		for i in range (0,self.ntrain):
+			self.train_features[i,:]=tmp[i]
+		tmp=[]
+		test_path=path+"set_test/c"+str(self.seg)+"test_"
+		print "reading test images"
+		for i in range(0,self.ntest):
+			filename=test_path+str(i+1)+".nii"
+			array=parse.voxels_from_image(filename,smoothening_width=self.smoothening,stride=[3,3,3])
+			tmp.append(preprocess.fft_features1D(array,self.ncubes))
+		self.test_features=np.zeros((self.ntest,tmp[0].size))
+		for i in range (0,self.ntest):
+			self.test_features[i,:]=tmp[i]
+		if(self.ncubes==1):
+			#Remove zero vaiance elements, to make the pca cheaper. This can only be done without cubes, since otherwise the format gets messed up
+			nonzeros=preprocess.get_nonzero_variance(self.train_features)
+			self.train_features=preprocess.remove_zero_variance(self.train_features,nonzeros)
+			self.test_features=preprocess.remove_zero_variance(self.test_features,nonzeros)
+		#self.train_features,self.test_features=preprocess.segwise_pca(self.train_features,self.test_features,self.ncubes,self.ntrain,self.ntest)
+
+				
